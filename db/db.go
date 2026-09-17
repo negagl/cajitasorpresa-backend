7cff55ecffeef15d65efe132e68050d30054effc
Nega García López <nega_gl@outlook.com>
2026-09-17 02:29:18 -0500

    Se crea conexion a BD

---
+++ db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"cajitasorpresa/models"
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	db, err := gorm.Open(sqlite.Open("cajitasorpresa.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Fallo al conectar a la BD")
+	}
+
+	if err := db.AutoMigrate(&models.Box{}, &models.IngredienteBox{}, &models.Extra{}, &models.Pedido{}, &models.PedidoExtra{}, &models.PersonalizacionPedido{}); err != nil {
+		log.Fatal("Fallo al realizar las migraciones")
+	}
+
+	DB = db
+}
