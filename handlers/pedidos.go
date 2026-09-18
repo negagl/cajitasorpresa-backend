3104e19ed52698b185b59a6eb2b22493a1699f4b
Nega García López <nega_gl@outlook.com>
2026-09-18 03:02:09 -0500

    Agregado handler de crear pedido

--- handlers/pedidos.go
+++ handlers/pedidos.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -62,4 +63,98 @@ func GetPedidosPorBoxId(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": pedidos})
+}
+
+func CrearPedido(c *gin.Context) {
+	var newPedidoInput CrearPedidoInput
+
+	if err := c.ShouldBindJSON(&newPedidoInput); err != nil {
+		log.Println("Error al parsear el body:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "json invalido"})
+		return
+	}
+
+	// Se busca el box primero
+	var box models.Box
+	if err := db.DB.First(&box, newPedidoInput.BoxID).Error; err != nil {
+		log.Println("Error al buscar el box:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error al buscar el box"})
+		return
+	}
+
+	precioTotal := box.PrecioBase
+
+	// Buscamos los extras ahora
+	for _, extraInput := range newPedidoInput.Extras {
+		var extra models.Extra
+		if err := db.DB.First(&extra, extraInput.ExtraID).Error; err != nil {
+			log.Println("Error al buscar los extras:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error al buscar los extras"})
+			return
+		}
+
+		// Calculamos el precio de cada extra y lo sumamos al total
+		precioTotal += extra.PrecioBase * float64(extraInput.Cantidad)
+	}
+
+	// Creamos el pedido
+	newPedido := models.Pedido{
+		NombreCliente:   newPedidoInput.NombreCliente,
+		TelefonoCliente: newPedidoInput.TelefonoCliente,
+		BoxID:           box.ID,
+		SaborJugo:       newPedidoInput.SaborJugo,
+		ConAzucar:       newPedidoInput.ConAzucar,
+		MensajeTarjeta:  newPedidoInput.MensajeTarjeta,
+		FotoURL:         newPedidoInput.FotoURL,
+		PrecioTotal:     precioTotal,
+		FechaPedido:     time.Now(),
+		FechaEntrega:    newPedidoInput.FechaEntrega,
+		Estado:          models.Pendiente,
+	}
+
+	if err := db.DB.Create(&newPedido).Error; err != nil {
+		log.Println("Error al crear el pedido:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error creando el pedido"})
+		return
+	}
+
+	// Creamos los extras
+	for _, extraInput := range newPedidoInput.Extras {
+		pedidoExtra := models.PedidoExtra{
+			PedidoID: newPedido.ID,
+			ExtraID:  extraInput.ExtraID,
+			Cantidad: extraInput.Cantidad,
+		}
+
+		if err := db.DB.Create(&pedidoExtra).Error; err != nil {
+			log.Println("Error al crear los pedidos extra:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error creando los pedidos extra"})
+			return
+		}
+	}
+
+	// Creamos las personalizaciones
+	for _, personalizacionInput := range newPedidoInput.Personalizacion {
+		personalizacion := models.PersonalizacionPedido{
+			PedidoID:  newPedido.ID,
+			Categoria: personalizacionInput.Categoria,
+			Eleccion:  personalizacionInput.Eleccion,
+		}
+
+		if err := db.DB.Create(&personalizacion).Error; err != nil {
+			log.Println("Error al crear las personalizaciones:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error creando las personalizaciones"})
+			return
+		}
+	}
+
+	// devolvemos el objeto completo con sus relaciones
+	var pedidoCreado models.Pedido
+	if err := db.DB.Preload("ItemsExtra").Preload("Personalizacion").First(&pedidoCreado, newPedido.ID).Error; err != nil {
+		log.Println("Error al buscar el pedido creado:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error al buscar el pedido creado"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data": pedidoCreado})
 }
