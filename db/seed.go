867801b51abf28d8283ff1e8186389dfd6db3995
Nega García López <nega_gl@outlook.com>
2026-09-22 02:04:45 -0500

    Agregar saltos de linea al seed

--- db/seed.go
+++ db/seed.go
@@ -10,64 +10,64 @@ func Seed() {
 	boxes := []models.Box{
 		{
 			Nombre: "Box amor y amistad",
-			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad)" +
-				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas" +
-				"- Granola, yogurt y fruta de temporada picada" +
-				"- Galletas" +
-				"- Dos chocolates" +
-				"- Tarjeta personalizada" +
-				"- Decoración" +
-				"- Foto impresa de la amistad o pareja",
+			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad) \n" +
+				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas \n" +
+				"- Granola, yogurt y fruta de temporada picada \n" +
+				"- Galletas \n" +
+				"- Dos chocolates \n" +
+				"- Tarjeta personalizada \n" +
+				"- Decoración \n" +
+				"- Foto impresa de la amistad o pareja \n",
 			PrecioBase: 70000,
 			Imagen:     "",
 		},
 		{
 			Nombre: "Box clasico",
-			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad)" +
-				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas" +
-				"- Fruta de temporada picada" +
-				"- Postre" +
-				"- Tarjeta personalizada con mensaje" +
-				"- Decoración",
+			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad) \n" +
+				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas \n" +
+				"- Fruta de temporada picada \n" +
+				"- Postre \n" +
+				"- Tarjeta personalizada con mensaje \n" +
+				"- Decoración \n",
 			PrecioBase: 67000,
 			Imagen:     "",
 		},
 		{
 			Nombre: "Box esencial",
-			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad)" +
-				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas / Deditos integrales" +
-				"- Postre / Mousse de mango sin azúcar" +
-				"- Chocolate / Galleta integral" +
-				"- Tarjeta personalizada" +
-				"- Decoración",
+			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad) \n" +
+				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas / Deditos integrales \n" +
+				"- Postre / Mousse de mango sin azúcar \n" +
+				"- Chocolate / Galleta integral \n" +
+				"- Tarjeta personalizada \n" +
+				"- Decoración \n",
 			PrecioBase: 55000,
 			Imagen:     "",
 		},
 		{
 			Nombre: "Box saludable",
-			Descripcion: "- Jugo natural sin azúcar (mora, maracuyá o naranja según disponibilidad)" +
-				"- Sandwich con jamón de pollo o de pavo y queso bajo en grasa con pan integral / Deditos integrales" +
-				"- Granola, yogurt griego y fruta de temporada picada" +
-				"- Barra de cereal Tosh" +
-				"- Mousse de mango sin azúcar" +
-				"- Tarjeta personalizada" +
-				"- Foto impresa",
+			Descripcion: "- Jugo natural sin azúcar (mora, maracuyá o naranja según disponibilidad) \n" +
+				"- Sandwich con jamón de pollo o de pavo y queso bajo en grasa con pan integral / Deditos integrales \n" +
+				"- Granola, yogurt griego y fruta de temporada picada \n" +
+				"- Barra de cereal Tosh \n" +
+				"- Mousse de mango sin azúcar \n" +
+				"- Tarjeta personalizada \n" +
+				"- Foto impresa \n",
 			PrecioBase: 75000,
 			Imagen:     "",
 		},
 		{
 			Nombre: "Box sorpresa premium",
-			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad)" +
-				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas / Bandejita de costeñitos (carimañolas, butifarras, bollo de mazorca y queso costeño)" +
-				"- Granola, yogurt y fruta de temporada picada" +
-				"- Galletas" +
-				"- Dos chocolates" +
-				"- Tarjeta personalizada" +
-				"- Decoración" +
-				"- Foto impresa de la amistad o pareja colgantes / Cuadro en madera con foto" +
-				"- Mini cake de cumpleaños" +
-				"- Hatsu en lata / Cerveza en lata" +
-				"- Mini bouquet de flores en claveles o margaritas / Mug personalizado con sobre de café",
+			Descripcion: "- Jugo natural con o sin azúcar (mora, maracuyá o naranja según disponibilidad) \n" +
+				"- Sandwich de jamón y queso con pan artesanal / Waffles con mermelada y fresas / Bandejita de costeñitos (carimañolas, butifarras, bollo de mazorca y queso costeño) \n" +
+				"- Granola, yogurt y fruta de temporada picada \n" +
+				"- Galletas \n" +
+				"- Dos chocolates \n" +
+				"- Tarjeta personalizada \n" +
+				"- Decoración \n" +
+				"- Foto impresa de la amistad o pareja colgantes / Cuadro en madera con foto \n" +
+				"- Mini cake de cumpleaños \n" +
+				"- Hatsu en lata / Cerveza en lata \n" +
+				"- Mini bouquet de flores en claveles o margaritas / Mug personalizado con sobre de café \n",
 			PrecioBase: 150000,
 			Imagen:     "",
 		},
